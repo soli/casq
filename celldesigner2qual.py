99e4bc3bf184c6f77f45435390658b2f92d1e872
Sylvain Soliman <Sylvain.Soliman@inria.fr>
2017-07-28 16:59:03 +0200

    saving layout to SBML

--- celldesigner2qual.py
+++ celldesigner2qual.py
@@ -8,11 +8,14 @@ import xml.etree.ElementTree as etree
 
 NS = {
     'sbml': 'http://www.sbml.org/sbml/level2/version4',
-    'cd': 'http://www.sbml.org/2001/ns/celldesigner'
+    'cd': 'http://www.sbml.org/2001/ns/celldesigner',
+    'sbml3': 'http://www.sbml.org/sbml/level3/version1/core',
+    'layout': 'http://www.sbml.org/sbml/level3/version1/layout/version1',
+    'qual': 'http://www.sbml.org/sbml/level3/version1/qual/version1',
 }
 
 
-def read_sbgn(filename):
+def read_celldesigner(filename):
     '''main file parsing function'''
     root = etree.parse(filename).getroot()
     tag = root.tag
@@ -38,12 +41,17 @@ def species_info(model):
     for species in model.findall(
             './sbml:annotation/cd:extension/*/*[@compartmentAlias]', NS):
         activity = species.find('.//cd:activity', NS).text
-        boundx = species.find('.//cd:bounds', NS).get('x')
-        boundy = species.find('.//cd:bounds', NS).get('y')
+        bound = species.find('.//cd:bounds', NS)
+        boundx = bound.get('x')
+        boundy = bound.get('y')
+        boundh = bound.get('h')
+        boundw = bound.get('w')
         nameconv[species.get('species')].update({
             'activity': activity,
             'x': boundx,
-            'y': boundy
+            'y': boundy,
+            'h': boundh,
+            'w': boundw,
         })
     return nameconv
 
@@ -64,15 +72,49 @@ def get_mods(cd_modifications):
     return mods
 
 
+def write_qual(filename, info):
+    '''write the SBML qual with layout file for our model'''
+    root = etree.Element('sbml', {
+        'level': '3', 'version': '1', 'layout:required': 'false',
+        'xmlns': NS['sbml3'], 'qual:required': 'true',
+        'xmlns:layout': NS['layout'], 'xmlns:qual': NS['qual'],
+    })
+    model = etree.Element('model', id="model_id")
+    clist = etree.SubElement(model, 'listOfCompartments')
+    etree.SubElement(clist, 'compartment', constant="true", id="comp1")
+    llist = etree.SubElement(model, 'layout:listOfLayouts')
+    layout = etree.SubElement(llist, 'layout:layout')
+    add_positions(layout, info)
+    root.append(model)
+    tree = etree.ElementTree(root)
+    tree.write(filename, "UTF-8", xml_declaration=True)
+
+
+def add_positions(layout, info):
+    '''create layout sub-elements'''
+    llist = etree.SubElement(layout, 'layout:listOfSpeciesGlyphs')
+    for species, data in info.items():
+        glyph = etree.SubElement(llist, 'layout:speciesGlyph',
+                                 {'layout:species': species})
+        box = etree.SubElement(glyph, 'layout:boundingBox')
+        etree.SubElement(box, 'layout:position',
+                         {'layout:x': data['x'], 'layout:y': data['y']})
+        etree.SubElement(box, 'layout:dimensions',
+                         {'layout:height': data['h'],
+                          'layout:width': data['w']})
+
+
 def main():
     '''run conversion using the CLI given first argument'''
-    if len(sys.argv) != 2:
-        print(f'Usage: [python3] {sys.argv[0]} <filename.sbgn>')
+    if len(sys.argv) != 3:
+        print(f'Usage: [python3] {sys.argv[0]} ' +
+              '<celldesignerinfile.xml> <sbmlqualoutfile.xml>')
         exit(1)
-    sbgnfile = sys.argv[1]
-    print(f'parsing {sbgnfile}…')
-    # read_sbgn(sbgnfile)
-    print(read_sbgn(sbgnfile))
+    celldesignerfile = sys.argv[1]
+    print(f'parsing {celldesignerfile}…')
+    info = read_celldesigner(celldesignerfile)
+    write_qual(sys.argv[2], info)
+    print(info)
 
 
 if __name__ == '__main__':  # pragma: no cover
