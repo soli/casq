1065c94adc052da8150dfbae458d08f84f3740c8
Sylvain Soliman <Sylvain.Soliman@inria.fr>
2019-07-11 17:09:41 +0200

    cleaner

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 with open("README.md", "r") as file:
-    readme = file.read()
+    README = file.read()
 
 with open("casq/__init__.py", "r") as file:
     for line in file.readlines():
@@ -12,30 +12,23 @@ setup(
     name="casq",
     version=version,
     description="CaSQ: Celldesigner as Sbml-Qual",
-    long_description=readme,
+    long_description=README,
     long_description_content="text/markdown",
     url="https://lifeware.inria.fr/~soliman/post/casq/",
-    project_urls={
-        "Source Code": "https://gitlab.inria.fr/soliman/sbgnpd2sbmlq",
-    },
+    project_urls={"Source Code": "https://gitlab.inria.fr/soliman/casq/"},
     license="MIT",
     packages=find_packages(),
     author="Sylvain Soliman",
     author_email="Sylvain.Soliman@inria.fr",
-    install_requires=[
-        "networkx>=2.2",
-        "loguru>=0.2.5",
-    ],
+    install_requires=["networkx>=2.2", "loguru>=0.2.5"],
     python_requires=">=3.5",
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
+    extras_require={"dev": ["black", "check-manifest", "flake8"]},
     include_package_data=True,
+    data_files=[("utils", ["utils/validate.sh"])],
     zip_safe=False,
-    entry_points={
-        "console_scripts": [
-            "casq = casq.celldesigner2qual:main",
-        ],
-    },
+    entry_points={"console_scripts": ["casq = casq.celldesigner2qual:main"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
