9dc23b0aca6ba2d4aa12cb9b401751457edfd766
Sylvain Soliman <Sylvain.Soliman@inria.fr>
2019-07-13 18:25:57 +0200

    test all XML files

--- test/test_casq.py
+++ test/test_casq.py
@@ -1,7 +1,8 @@
 """Tests for CaSQ."""
 
+import glob
 import os
-import sys
+import unittest.mock
 
 from casq.celldesigner2qual import main
 from casq.utils import validate
@@ -9,15 +10,15 @@ from casq.utils import validate
 import pytest  # type: ignore
 
 
-@pytest.mark.parametrize("infile", ["map_mastcell.xml"])
+@pytest.mark.parametrize(
+    "infile",
+    glob.glob(os.path.join(str(os.path.dirname(os.path.realpath(__file__))), "*.xml")),
+)
 def test_casq_produces_valid_files(tmp_path, infile):
     """Check if the files we produce are valid."""
-    outfile = os.path.join(str(tmp_path), os.path.splitext(infile)[0] + ".sbml")
-    infile = os.path.join(str(os.path.dirname(os.path.realpath(__file__))), infile)
-    store_argv = sys.argv
-    sys.argv = ["casq", infile, outfile]
-    try:
+    outfile = os.path.join(
+        str(tmp_path), os.path.splitext(os.path.basename(infile))[0] + ".sbml"
+    )
+    with unittest.mock.patch("sys.argv", ["casq", infile, outfile]):
         main()
-    finally:
-        sys.argv = store_argv
     assert validate(outfile) == "OK"
