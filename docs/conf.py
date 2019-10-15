03e4661f89b794e5736e75f44afb6708f3b18d5a
Sylvain Soliman <Sylvain.Soliman@inria.fr>
2019-10-15 13:41:31 +0200

    manifest

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,16 @@ html_theme = 'alabaster'
 
 html_logo = 'CaSQ_logo.jpg'
 
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+        # 'donate.html',
+    ]
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
