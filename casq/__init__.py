73447e191c793d5b392ce17bbba289cfa4f8d9e7
Sylvain Soliman <Sylvain.Soliman@inria.fr>
2019-10-17 16:42:33 +0200

    version bump instructions

--- casq/__init__.py
+++ casq/__init__.py
@@ -17,4 +17,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+# Don't forget to git tag v<version> && git push --tags
+# and to python3 setup.py sdist bdist_wheel && python3 -m twine upload dist/*
 version = "0.7.1"  # pylint: disable=invalid-name
