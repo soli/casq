7fde13a058e59aee33d7832fc329aa294de445d7
Sylvain Soliman <Sylvain.Soliman@inria.fr>
2021-02-26 11:07:14 +0100

    flake8 and black

--- casq/bmaExport.py
+++ casq/bmaExport.py
@@ -18,207 +18,259 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import json
 
-class counter():
-    def __init__(self,first):
+
+class counter:
+    def __init__(self, first):
         self.value = first
+
     def next(self):
         result = self.value
         self.value += 1
-        return(result)
+        return result
 
-class booleanFormulaBuilder():
+
+class booleanFormulaBuilder:
     def __init__(self):
         self.value = "0"
-        self.transition ="1"
-    def addActivator(self,vid):
-        self.transition = "(min(var({vid}),{current}))".format(vid = vid, current = self.transition)
-    def addInhibitor(self,vid):
-        self.transition = "(min(1-var({vid}),{current}))".format(vid = vid, current = self.transition)
+        self.transition = "1"
+
+    def addActivator(self, vid):
+        self.transition = "(min(var({vid}),{current}))".format(
+            vid=vid, current=self.transition
+        )
+
+    def addInhibitor(self, vid):
+        self.transition = "(min(1-var({vid}),{current}))".format(
+            vid=vid, current=self.transition
+        )
+
     def addTransition(self):
         self.transition = "1"
-    def addCatalysis(self,vidList):
+
+    def addCatalysis(self, vidList):
         base = "0"
         for vid in vidList:
-            base = "(max(var({vid}),{base}))".format(vid=vid,base=base)
+            base = "(max(var({vid}),{base}))".format(vid=vid, base=base)
         self.value = "(min({base},{current}))".format(base=base, current=self.value)
+
     def finishTransition(self):
-        self.value = "(max({transition},{current}))".format(transition=self.transition, current=self.value)
+        self.value = "(max({transition},{current}))".format(
+            transition=self.transition, current=self.value
+        )
         self.transition = "1"
 
-class multiStateFormulaBuilder():
+
+class multiStateFormulaBuilder:
     def __init__(self):
         self.value = ""
-    def addActivator(self,vid):
+
+    def addActivator(self, vid):
         pass
-    def addInhibitor(self,vid):
+
+    def addInhibitor(self, vid):
         pass
-    def addCatalysis(self,vidList):
+
+    def addCatalysis(self, vidList):
         pass
+
     def addTransition(self):
         pass
+
     def finishTransition(self):
         pass
 
+
 def colourMap(n):
-    if   n == 0: return("BMA_Green")
-    elif n == 1: return("BMA_Orange")
-    elif n == 2: return("BMA_Purple")
-    elif n == 3: return("BMA_Mint")
-    else: return(None)
+    if n == 0:
+        return "BMA_Green"
+    elif n == 1:
+        return "BMA_Orange"
+    elif n == 2:
+        return "BMA_Purple"
+    elif n == 3:
+        return "BMA_Mint"
+    else:
+        return None
 
-def bma_relationship(source,target,idMap,count,which="Activator"):
+
+def bma_relationship(source, target, idMap, count, which="Activator"):
     result = {
-                'ToVariable': idMap[target],
-                'Type': which, 
-                'FromVariable': idMap[source], 
-                'Id': count.next()
-                }
-    return(result)
+        "ToVariable": idMap[target],
+        "Type": which,
+        "FromVariable": idMap[source],
+        "Id": count.next(),
+    }
+    return result
 
-def get_relationships(info,idMap,count,granularity,ignoreSelfLoops):
+
+def get_relationships(info, idMap, count, granularity, ignoreSelfLoops):
     relationships = []
     allFormulae = {}
     for item in info.keys():
-        #skip if there are no transitions
-        if len(info[item]["transitions"])==0: continue
+        # skip if there are no transitions
+        if len(info[item]["transitions"]) == 0:
+            continue
         product = item
-        if granularity == 1: 
+        if granularity == 1:
             formula = booleanFormulaBuilder()
         else:
             formula = multiStateFormulaBuilder()
-        #variables may be missing from the "simplified" model. Test for variable in the ID map before appending
+        # variables may be missing from the "simplified" model. Test for variable in the ID map before appending
         for transition in info[item]["transitions"]:
             formula.addTransition()
-            #reactant
+            # reactant
             for reactant in transition[1]:
-                if ignoreSelfLoops and reactant == product: continue
+                if ignoreSelfLoops and reactant == product:
+                    continue
                 if reactant in idMap:
-                    relationships.append(bma_relationship(reactant,product,idMap,count))
+                    relationships.append(
+                        bma_relationship(reactant, product, idMap, count)
+                    )
                     formula.addActivator(idMap[reactant])
                 else:
                     pass
-            #now modifiers
-            if len(transition[2]) == 0: continue
+            # now modifiers
+            if len(transition[2]) == 0:
+                continue
             modifiers = transition[2]
-            #catalysts are a special case
+            # catalysts are a special case
             catalysts = []
-            for (impact,m) in modifiers:
-                if ignoreSelfLoops and m == product: continue
+            for (impact, m) in modifiers:
+                if ignoreSelfLoops and m == product:
+                    continue
                 if m in idMap:
                     if impact == "CATALYSIS" or impact == "UNKNOWN_CATALYSIS":
                         catalysts.append(idMap[m])
-                        relationships.append(bma_relationship(m,product,idMap,count))
+                        relationships.append(bma_relationship(m, product, idMap, count))
                     else:
                         pass
                 else:
                     pass
             formula.addCatalysis(catalysts)
-            #everything else
-            for (impact,m) in modifiers:
-                if ignoreSelfLoops and m == product: continue
+            # everything else
+            for (impact, m) in modifiers:
+                if ignoreSelfLoops and m == product:
+                    continue
                 if m in idMap:
-                    if impact == "UNKNOWN_INHIBITION" or impact == "INHIBITION" :
-                        relationships.append(bma_relationship(m,product,idMap,count,"Inhibitor"))
+                    if impact == "UNKNOWN_INHIBITION" or impact == "INHIBITION":
+                        relationships.append(
+                            bma_relationship(m, product, idMap, count, "Inhibitor")
+                        )
                         formula.addInhibitor(idMap[m])
                     elif impact == "CATALYSIS" or impact == "UNKNOWN_CATALYSIS":
-                        pass #deal with this earlier
+                        pass  # deal with this earlier
                     else:
-                        #treat all other modifiers as reactants
-                        relationships.append(bma_relationship(m,product,idMap,count))
+                        # treat all other modifiers as reactants
+                        relationships.append(bma_relationship(m, product, idMap, count))
                         formula.addActivator(idMap[m])
                 else:
                     pass
             formula.finishTransition()
         allFormulae[item] = formula.value
-    return(relationships, allFormulae)
+    return (relationships, allFormulae)
+
 
 def translateGreek(name):
-    greek_alphabet = 'ΑαΒβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφΧχΨψΩω'
-    latin_alphabet = 'AaBbGgDdEeZzHhJjIiKkLlMmNnXxOoPpRrSssTtUuFfQqYyWw'
+    greek_alphabet = "ΑαΒβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφΧχΨψΩω"
+    latin_alphabet = "AaBbGgDdEeZzHhJjIiKkLlMmNnXxOoPpRrSssTtUuFfQqYyWw"
     greek2latin = str.maketrans(greek_alphabet, latin_alphabet)
-    return(name.translate(greek2latin))
+    return name.translate(greek2latin)
+
 
 def depunctuate(name):
-    #name.replace(' ' , '_').replace(',' , '_').replace('-', '_').replace('(','').replace(')','').replace('+','').replace(':','').replace('/','').replace('\\','').replace("'",'')
-    badChars =     ' ,-()+:/\\\''
-    alternatives = '__________'
-    cleanup = str.maketrans(badChars,alternatives)
-    return(name.translate(cleanup))
+    # name.replace(' ' , '_').replace(',' , '_').replace('-', '_').replace('(','').replace(')','').replace('+','').replace(':','').replace('/','').replace('\\','').replace("'",'')
+    badChars = " ,-()+:/\\'"
+    alternatives = "__________"
+    cleanup = str.maketrans(badChars, alternatives)
+    return name.translate(cleanup)
+
 
 def cleanName(name):
     noPunctuation = depunctuate(name)
     result = translateGreek(noPunctuation)
-    return(result)
+    return result
+
 
 def bma_model_variable(vid, infoVariable, formulaDict, v, granularity):
     if v in formulaDict:
-        formula=formulaDict[v]
+        formula = formulaDict[v]
     else:
-        #Assume that nodes with no incoming edges are active
+        # Assume that nodes with no incoming edges are active
         formula = str(granularity)
     result = {
-        'Name':cleanName(infoVariable["name"]),
-        'Id':vid,
-        'RangeFrom':0,
-        'RangeTo':granularity,
-        'Formula': formula
-        }
-    return(result)
+        "Name": cleanName(infoVariable["name"]),
+        "Id": vid,
+        "RangeFrom": 0,
+        "RangeTo": granularity,
+        "Formula": formula,
+    }
+    return result
 
-def bma_layout_variable(vid, infoVariable,fill=None,description=""):
+
+def bma_layout_variable(vid, infoVariable, fill=None, description=""):
     result = {
-        'Id': vid,
-        'Name':cleanName(infoVariable["name"]),
-        'Type':"Constant",
-        'ContainerId':0,
-        'PositionX':float(infoVariable["x"]),
-        'PositionY':float(infoVariable["y"]),
-        'CellY':0,
-        'CellX':0,
-        'Angle':0,
-        'Description':description,
-        }
-    if fill != None: result['Fill'] = fill
-    return(result)
+        "Id": vid,
+        "Name": cleanName(infoVariable["name"]),
+        "Type": "Constant",
+        "ContainerId": 0,
+        "PositionX": float(infoVariable["x"]),
+        "PositionY": float(infoVariable["y"]),
+        "CellY": 0,
+        "CellX": 0,
+        "Angle": 0,
+        "Description": description,
+    }
+    if fill is not None:
+        result["Fill"] = fill
+    return result
 
-def write_bma(
-    filename: str, info, granularity=1, ignoreSelfLoops=False
-):
+
+def write_bma(filename: str, info, granularity=1, ignoreSelfLoops=False):
     # pylint: disable=too-many-arguments, too-many-locals
     """Write the BMA json with layout file for our model."""
-    #granularity must be a non-zero natural
-    assert(granularity>0)
+    # granularity must be a non-zero natural
+    assert granularity > 0
 
-    #calculate the compartments for colours; four largest compartments are coloured BMA colours, all else default
+    # calculate the compartments for colours; four largest compartments are coloured BMA colours, all else default
     compartments = {}
     for k in info.keys():
-        location = info[k]['compartment']
+        location = info[k]["compartment"]
         if location in compartments:
             compartments[location] += 1
         else:
             compartments[location] = 1
     compList = list(compartments.items())
-    compList.sort(key= lambda i: -i[1])
+    compList.sort(key=lambda i: -i[1])
     compartmentColour = {}
     for i in range(len(compartments)):
         compartmentColour[compList[i][0]] = colourMap(i)
 
     idGenerator = counter(1)
-    idMap = dict([(k,idGenerator.next()) for k in info.keys()])
+    idMap = dict([(k, idGenerator.next()) for k in info.keys()])
 
-    rm,formula = get_relationships(info, idMap, idGenerator, granularity, ignoreSelfLoops)
+    rm, formula = get_relationships(
+        info, idMap, idGenerator, granularity, ignoreSelfLoops
+    )
 
-    vm = [bma_model_variable(idMap[v],info[v],formula,v,granularity) for v in info.keys()]
-    vl = [bma_layout_variable(idMap[v],info[v],compartmentColour[info[v]['compartment']],info[v]['compartment']) for v in info.keys()]
+    vm = [
+        bma_model_variable(idMap[v], info[v], formula, v, granularity)
+        for v in info.keys()
+    ]
+    vl = [
+        bma_layout_variable(
+            idMap[v],
+            info[v],
+            compartmentColour[info[v]["compartment"]],
+            info[v]["compartment"],
+        )
+        for v in info.keys()
+    ]
 
-    model = {"Name":"CaSQ-BMA","Variables":vm,"Relationships":rm}
-    layout = {"Variables":vl,"Containers":[],"Description":""}
-    ltl = {
-        "states": [],
-        "operations": []
-    }
-    universe = {"Model":model,"Layout":layout,'ltl':ltl}
+    model = {"Name": "CaSQ-BMA", "Variables": vm, "Relationships": rm}
+    layout = {"Variables": vl, "Containers": [], "Description": ""}
+    ltl = {"states": [], "operations": []}
+    universe = {"Model": model, "Layout": layout, "ltl": ltl}
 
-    json_object = json.dumps(universe, indent = 4) 
-    with open(filename,"w") as outfile:
+    json_object = json.dumps(universe, indent=4)
+    with open(filename, "w") as outfile:
         outfile.write(json_object)
